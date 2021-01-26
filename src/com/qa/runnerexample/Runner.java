0b44947359ad992b7a22437b6ffddb6c12744de4
Cameron Ofoluwa <COfoluwa@qa.com>
2021-01-26 10:01:56 +0000

    update

--- src/com/qa/runnerexample/Runner.java
+++ src/com/qa/runnerexample/Runner.java
@@ -1,5 +1,8 @@
 package com.qa.runnerexample;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Runner {
 
 	public static void main(String[] args) {
@@ -9,15 +12,14 @@ public class Runner {
 		list[1] = new Person("James", "Rod", "Teacher");
 		list[2] = new Person("Sarah", "Allan", "Doctor");
 		list[3] = new Person("Becky", "Charles", "Chef");
-		list[4] = new Person("Bob", "Smith", "Driver");
+		list[4] = new Person("Rod", "die", "rich");
 		
-		findUser(list, "Cameron");
+		createPerson(list);
 	}
 
 	public static void printNames(Person[] listname) {
-		for(int i = 0; i < listname.length; i++) {
-			System.out.println("INDEX = " + i + " " + listname[i].toString());
-		}
+        for (Person i : listname)
+            System.out.println(i);
 	}
 
 	public static void findUser(Person[] listname, String username) {
@@ -28,5 +30,19 @@ public class Runner {
 				System.out.println("Cannot find that user");
 			}
 		}
+	}
+	
+	public static void createPerson(Person[] listname) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your first name : ");
+        String fname = scan.nextLine();	
+        System.out.println("Enter your last name : ");
+        String lname = scan.nextLine();		
+        System.out.println("Enter your job type : ");
+        String job = scan.nextLine();
+        listname = Arrays.copyOf(listname, listname.length + 1);
+        listname[listname.length -1] = new Person(fname, lname, job);
+        for (Person i : listname)
+            System.out.println(i);
 	}
 }
